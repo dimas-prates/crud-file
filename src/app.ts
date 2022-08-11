6028c82a1688b4cee7a53dd3c1f6992e1b58ba07
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-08-11 19:42:11 -0300

    :sparkles: feat(app.ts): Create server file + add middlewares

---
+++ src/app.ts
@@ -0,0 +1,8 @@
+import express from 'express'
+import "dotenv/config"
+import routes from './routes'
+const app = express();
+const port = process.env.PORT || 3000
+app.use(express.json())
+app.use(routes)
+app.listen(port, () => { console.log(`\n Server's running on port ${port} \n`) })
